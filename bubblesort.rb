8992636abd170e859744217e937cabfa24e28b6b
Michael Dijamco <mdd233@cornell.edu>
2015-09-05 13:53:22 -0400

    created test cases

---
+++ bubblesort.rb
@@ -0,0 +1,7 @@
+def bubble_sort(array)
+
+end
+
+def bubble_sort_by(array)
+
+end
---
+++ spec/bubblesort_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../bubblesort'
+
+describe 'bubblesort' do
+	it 'sorts an array of numbers using the bubblesort algorithm' do
+		bubble_sort([4,3,78,2,0,2]).should == [0,2,2,3,4,78]
+	end
+
+	it 'sorts an array of strings using the bubblesort algorithm' do
+		bubble_sort(['zulu', 'tango','alpha','foxtrot','echo']).should == %w(alpha echo foxtrot tango zulu)
+	end
+end
+
+describe 'bubble_sort_by' do
+	it 'sorts an array based on a block' do
+		 bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}.should == ["hi", "hey", "hello"]
+    end
+
+end
